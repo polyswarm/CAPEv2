2a8bcad004f4db4c44f4b165ef9c4b980a9bd771
Michael Bradford <michael.bradford@polyswarm.io>
2024-03-13 09:49:09 -0700

    use shortcut

--- analyzer/windows/modules/packages/edge.py
+++ analyzer/windows/modules/packages/edge.py
@@ -5,9 +5,13 @@ class Edge(Package):
     """Edge analysis package."""
 
     PATHS = [
-        ("ProgramFiles", "Microsoft", "Edge", "Application", "msedge.exe"),
-        ("ProgramFiles(x86)", "Microsoft", "EdgeCore", "112.0.1722.58", "msedge.exe")
+        ("Users", "maxine", "Desktop", "Microsoft Edge.lnk")
     ]
 
     def start(self, url):
-        return self.execute('"{}"'.format(url), "", '"{}"'.format(url))
+        edge = self.get_path("Microsoft Edge.lnk")
+        args = [
+            "file:///{}".format(url.replace('\\', '/')),
+        ]
+        args = " ".join(args)
+        return self.execute(edge, args, url)
