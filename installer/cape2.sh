f9ec0330977fe03231ec72747ebe831cb52f7187
Michael Bradford <michael.bradford@polyswarm.io>
2024-04-18 13:07:55 -0700

    use our fork of community package

--- installer/cape2.sh
+++ installer/cape2.sh
@@ -1169,7 +1169,7 @@ function install_CAPE() {
       crudini --set conf/reporting.conf runstatistics enabled yes
     fi
 
-    python3 utils/community.py -waf -cr
+    python3 utils/community.py -waf -cr --url "https://github.com/polyswarm/CAPESandbox-community/archive/develop.tar.gz"
 
     # Configure direct internet connection
     sudo echo "400 ${INTERNET_IFACE}" >> /etc/iproute2/rt_tables
